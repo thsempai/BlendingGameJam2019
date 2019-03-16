e5007fb60773f9ab9794e07f9f5bccfe6d12196a
Jeremy <jeremy.grandclaudon@gmail.com>
2019-03-17 00:07:10 +0100

    setup scene V1

---
+++ BlendingGameJam2019/Assets/#Project/Scripts/SetupButtons.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupButtons : MonoBehaviour
+{
+    public void SetupPlayer(int number)
+    {
+        PlayerPrefs.SetInt("players_number", number);
+    }
+}
